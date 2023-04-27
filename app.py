27e83849b7bce46a76115074e2be9b3de1c95f3b
Richard <71686985+richie-03@users.noreply.github.com>
2023-04-27 15:49:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,16 +8,18 @@ option = st.radio("Select an option:", ("wo_data", "w_data"))
 
 # Generate a response based on the selected option
 if option == "wo_data":
-    response = generate_response(query,option)
+    response = generate_response(query)
 else:
     # Create a file upload component for the document upload
     document_upload = st.file_uploader("Upload a document:")
     if document_upload is None:
         response = "Please upload a document."
     else:
-        doc = document_upload.read()
-        response = generate_response(query,option,doc)
+        doc_contents = document_upload.read()
+        doc = document_upload.name
+        with open(f"{doc}","wb") as f:
+            f.write(doc_contents)
+        response = generate_response(query,doc)
 
 # Display the response in a box
 st.info(response)
-
