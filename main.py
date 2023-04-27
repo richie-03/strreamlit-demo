9a09f91cb55bd1f528fe011981b449b56b858916
Richard <71686985+richie-03@users.noreply.github.com>
2023-04-27 15:38:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,17 +9,17 @@ from langchain.chains import ConversationalRetrievalChain
 from langchain.document_loaders import PyPDFLoader
 
 from credentials import *
-
-question = "What is physicians brain digital twin?"
-
-llm = AzureOpenAI(deployment_name="text-davinci-003", model_name="text-davinci-003")
-wo_data_response = llm(question)
-
-loader = PyPDFLoader("cisco.pdf")
-pages = loader.load_and_split()
-embeddings = OpenAIEmbeddings(model="text-embedding-ada-002")
-vectorstore = FAISS.from_documents(pages, embeddings)
-
-qa = ConversationalRetrievalChain.from_llm(AzureChatOpenAI(deployment_name="GPT35-Demo1", model_name="gpt-35-turbo", temperature=0), vectorstore.as_retriever())
-w_data_response = qa({"question": question, "chat_history": ""})
-
+def generate_response(*args):
+  if len(args) == 1:
+    query = args[0]
+  else:
+    query = args[0]
+    doc = args[1]
+  llm = AzureOpenAI(deployment_name="text-davinci-003", model_name="text-davinci-003")
+  wo_data_response = llm(query)
+  loader = PyPDFLoader("cisco.pdf")
+  pages = loader.load_and_split()
+  embeddings = OpenAIEmbeddings(model="text-embedding-ada-002")
+  vectorstore = FAISS.from_documents(pages, embeddings)
+  qa = ConversationalRetrievalChain.from_llm(AzureChatOpenAI(deployment_name="GPT35-Demo1", model_name="gpt-35-turbo", temperature=0), vectorstore.as_retriever())
+  w_data_response = qa({"question": question, "chat_history": ""})
