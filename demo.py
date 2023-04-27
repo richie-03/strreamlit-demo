5e89c4647213523a5124683ccaa60e00d40d8c64
Richard <71686985+richie-03@users.noreply.github.com>
2023-04-27 15:06:59 +0530

    Update and rename test.py to demo.py

--- demo.py
+++ demo.py
@@ -1,22 +1,22 @@
 import streamlit as st
-
+from generate import *
 # Create a text input field for the string input
-string_input = st.text_input("Enter a string input:")
+query = st.text_input("Enter your query:")
 
 # Create a toggle button to select an option
-option_selected = st.radio("Select an option:", ("Option 1", "Option 2"))
+option = st.radio("Select an option:", ("wo_data", "w_data"))
 
 # Generate a response based on the selected option
-if option_selected == "Option 1":
-    response = f"You selected {option_selected}. Your string input is: {string_input}."
+if option == "wo_data":
+    response = generate_response(query,option)
 else:
     # Create a file upload component for the document upload
     document_upload = st.file_uploader("Upload a document:")
     if document_upload is None:
         response = "Please upload a document."
     else:
-        document_contents = document_upload.read()
-        response = f"You selected {option_selected}. Your document contents are:\n\n{document_contents}"
+        doc = document_upload.read()
+        response = generate_response(query,option,doc)
 
 # Display the response in a box
 st.info(response)
