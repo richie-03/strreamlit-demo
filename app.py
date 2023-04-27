3dc85a8a4d5ff884544d39ee9f983da35828b362
Richard <71686985+richie-03@users.noreply.github.com>
2023-04-27 16:33:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,24 @@
+import os
+import openai
+
+openai.api_type = "azure"
+openai.api_base = "https://gpt-demo1.openai.azure.com/"
+openai.api_version = "2022-12-01"
+
+# with open("openapi_key.txt", 'r') as file:
+#    api_key = file.read().replace('\n', '')
+api_key = "d6420559b1154f5d82f8364ec4c77b55"
+os.environ["OPENAI_API_KEY"] = api_key
+os.environ["AZURE_OPENAI_API_KEY"] = api_key
+os.environ["AZURE_OPENAI_ENDPOINT"] = "https://gpt-demo1.openai.azure.com/"
+os.environ["OPENAI_ENGINES"] = "GPT-35-Demo1"
+os.environ["OPENAI_EMBEDDINGS_ENGINE_DOC"] = "text-embedding-ada"
+os.environ["OPENAI_EMBEDDINGS_ENGINE_QUERY"] = "text-embedding-ada"
+os.environ["OPENAI_API_BASE"] = "https://gpt-demo1.openai.azure.com"
+os.environ["OPENAI_ENGINES"] = "GPT35-Demo1"
+os.environ["OPENAI_API_VERSION"] = "2023-03-15-preview"
+
+
 import streamlit as st
 from main import *
 # Create a text input field for the string input
