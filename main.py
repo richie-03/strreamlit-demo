8fa7e561051d24e4cb712a917cbb27467b410a17
Richard <71686985+richie-03@users.noreply.github.com>
2023-04-27 12:41:37 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+import os
+import openai
+import langchain
+from langchain.llms import AzureOpenAI
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chat_models import AzureChatOpenAI
+from langchain.chains import ConversationalRetrievalChain
+from langchain.document_loaders import PyPDFLoader
+
+from credetials import *
+
+question = "What is physicians brain digital twin?"
+
+llm = AzureOpenAI(deployment_name="text-davinci-003", model_name="text-davinci-003")
+wo_data_response = llm(question)
+
+loader = PyPDFLoader("docs/KGSWC_CameraReady_Paper18.pdf")
+pages = loader.load_and_split()
+embeddings = OpenAIEmbeddings(model="text-embedding-ada-002")
+vectorstore = FAISS.from_documents(pages, embeddings)
+
+qa = ConversationalRetrievalChain.from_llm(AzureChatOpenAI(deployment_name="GPT35-Demo1", model_name="gpt-35-turbo", temperature=0), vectorstore.as_retriever())
+w_data_response = qa({"question": question, "chat_history": ""})
+
